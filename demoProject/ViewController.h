e8f839a729a2d8a616fb15983fdc9a72f0278c5e
rnjailamba <rnjailamba12@gmail.com>
2016-08-27 06:11:29 +0530

    Initial Commit

---
+++ demoProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demoProject
+//
+//  Created by alisha bhatia on 27/08/16.
+//  Copyright © 2016 alisha bhatia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ demoProjectTests/demoProjectTests.m
@@ -0,0 +1,39 @@
+//
+//  demoProjectTests.m
+//  demoProjectTests
+//
+//  Created by alisha bhatia on 27/08/16.
+//  Copyright © 2016 alisha bhatia. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface demoProjectTests : XCTestCase
+
+@end
+
+@implementation demoProjectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
