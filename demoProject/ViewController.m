f97ba9931099bbcbbd759dc53ea6317165f0af73
rnjai <rnjailamba12@gmail.com>
2016-08-27 12:35:43 +0530

    completely equispaced cells in vertical collection view accross devices

--- demoProject/ViewController.m
+++ demoProject/ViewController.m
@@ -11,6 +11,7 @@
 @interface ViewController () <UICollectionViewDelegate, UICollectionViewDataSource>
 
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (weak, nonatomic) IBOutlet UICollectionView *verticalCollectionView;
 
 @end
 
@@ -21,6 +22,10 @@
     [self.navigationController setNavigationBarHidden:YES];
     self.collectionView.delegate = self;
     self.collectionView.dataSource = self;
+    
+    self.verticalCollectionView.delegate = self;
+    self.verticalCollectionView.dataSource = self;
+
     [self registerNib];
     self.automaticallyAdjustsScrollViewInsets = NO;
     // Do any additional setup after loading the view, typically from a nib.
@@ -28,6 +33,7 @@
 
 -(void)registerNib{
     [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cell"];
+    [self.verticalCollectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cellImage"];
 }
 
 -(BOOL)prefersStatusBarHidden{
@@ -41,47 +47,75 @@
 
 #pragma UICollectionViewDelegate
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
-    
-    return 1;
+    if(collectionView == self.collectionView)
+        return 1;
+    else
+        return 1;
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
-    
-    return 3;
+    if(collectionView == self.collectionView)
+        return 3;
+    else
+        return 15;
 }
 
 -(UICollectionViewCell*)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
-    
-    UICollectionViewCell *cell = [self.collectionView dequeueReusableCellWithReuseIdentifier:@"cell" forIndexPath:indexPath];
-    UIButton *button = [[UIButton alloc]initWithFrame:CGRectMake(0,0, self.collectionView.frame.size.width/2,self.collectionView.frame.size.height)];
+    if(collectionView == self.collectionView){
+        UICollectionViewCell *cell = [self.collectionView dequeueReusableCellWithReuseIdentifier:@"cell" forIndexPath:indexPath];
+        UIButton *button = [[UIButton alloc]initWithFrame:CGRectMake(0,0, self.collectionView.frame.size.width/2,self.collectionView.frame.size.height)];
+        cell.backgroundColor = [self randomNiceColor];
+        if(indexPath.row == 0){
+            [button setTitle:@"Baha'i" forState:UIControlStateNormal];
+        }
+        if(indexPath.row == 1){
+            [button setTitle:@"Baha'i1" forState:UIControlStateNormal];
+        }
+        if(indexPath.row == 2){
+            [button setTitle:@"Baha'i2" forState:UIControlStateNormal];
+        }
+        [button setEnabled:NO];
+        [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [cell.contentView addSubview:button];
+        return cell;
+    }
+    else{
+        UICollectionViewCell *cell = [self.verticalCollectionView dequeueReusableCellWithReuseIdentifier:@"cellImage" forIndexPath:indexPath];
+        cell.backgroundColor = [self randomNiceColor];
+        return cell;
+    }
 
-    if(indexPath.row == 0){
-        cell.backgroundColor = [self randomNiceColor];
-        [button setTitle:@"Baha'i" forState:UIControlStateNormal];
-    }
-    if(indexPath.row == 1){
-        cell.backgroundColor = [self randomNiceColor];
-        [button setTitle:@"Baha'i1" forState:UIControlStateNormal];
-    }
-    if(indexPath.row == 2){
-        cell.backgroundColor = [self randomNiceColor];
-        [button setTitle:@"Baha'i2" forState:UIControlStateNormal];
-    }
-    [button setEnabled:NO];//so that did select works
-    [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [cell.contentView addSubview:button];
-    return cell;
     
 }
 
 - (CGSize)collectionView:(UICollectionView *)collectionView
                   layout:(UICollectionViewLayout *)collectionViewLayout
   sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
-    return CGSizeMake(self.collectionView.frame.size.width/2, self.collectionView.frame.size.height);
+    if(collectionView == self.collectionView){
+        return CGSizeMake(self.collectionView.frame.size.width/2, self.collectionView.frame.size.height);
+    }
+    else{
+        return CGSizeMake(self.verticalCollectionView.frame.size.width/2 - 3, self.verticalCollectionView.frame.size.height/4);
+    }
+}
+
+- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
+    if(collectionView == self.collectionView){
+        return UIEdgeInsetsMake(0, 0, 0, 0);
+    }
+    else{
+        NSLog(@"section : %ld",(long)section);
+        return UIEdgeInsetsMake(2, 2, 0, 2);
+    }
 }
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
-    
+    if(collectionView == self.collectionView){
+        
+    }
+    else{
+        
+    }
 }
 
 - (UIColor *)randomNiceColor
